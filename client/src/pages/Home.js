9327a036a3c7961e5fb3fed14ae658b1f0acdbfb
OmarGuajardo <omar.guajardog@outlook.com>
2021-02-07 00:06:49 -0600

    visual re-design of home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaReact } from "react-icons/fa";
+import Button from "@material-ui/core/Button";
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to the CSTEM Resources Interface</h1>
+    <div className="background">
+      <FaReact className="login-icon" />
+      <FaReact id="top-right" className="login-icon-background" />
+      <FaReact id="bottom-left" className="login-icon-background" />
+      <h1>CSTEM</h1>
+      <h1>RESOURCES</h1>
       <Link to="/login">
-        <button>Get Started</button>
+        <Button>Get Started</Button>
       </Link>
     </div>
   );
