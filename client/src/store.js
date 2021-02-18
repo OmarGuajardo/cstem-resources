25a673b31732ca4debec978fdd47318d84d1cc6c
OmarGuajardo <omar.guajardog@outlook.com>
2021-02-18 16:08:42 -0600

    fixed redux logger bug

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,18 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+const devTools =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    : null;
+
 const initialState = {};
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(thunk),
-    // applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(thunk), devTools)
 );
 
 export default store;
