fc322e45a4b4a1bccb2d03061a03c5dddeb375d7
OmarGuajardo <omar.guajardog@outlook.com>
2021-02-18 16:17:14 -0600

    new fix for redux dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,19 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-const devTools =
-  process.env.NODE_ENV === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-    : null;
 
 const initialState = {};
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(thunk), devTools)
+  compose(
+    applyMiddleware(thunk),
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+      ? (a) => a
+      : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
